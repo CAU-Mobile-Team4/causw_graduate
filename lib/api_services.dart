02a7eb05b0c87b97e665f8238c57cad2411a52b2
manleKim <kk410910@naver.com>
2023-11-21 10:12:10 +0900

    feat: api 작성

---
+++ lib/api_services.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+import 'package:causw_graduate/schedule.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = 'http://localhost:8080';
+
+  //일정 반환
+  static Future<List<Schedule>> getSchedules(int studentID) async {
+    final url = Uri.parse('$baseUrl/schedule/$studentID');
+    final response = await http.get(url);
+    if (response.statusCode != 200) {
+      throw Error();
+    }
+    final List<dynamic> schedules = jsonDecode(response.body);
+    List<Schedule> scheduleInstances =
+        schedules.map((schedule) => Schedule.fromJson(schedule)).toList();
+    return scheduleInstances;
+  }
+
+//일정 추가(nlp)
+  static void addScheduleByNlp(int studentID, String statement) async {
+    final url = Uri.parse('$baseUrl/schedule/$studentID');
+    final response = await http.post(url, body: statement);
+
+    if (response.statusCode != 200) {
+      throw Error();
+    }
+    return;
+  }
+
+  //일정 추가
+  static void addSchedule(int studentID, Schedule schedule) async {
+    final url = Uri.parse('$baseUrl/schedule/$studentID');
+    final body = jsonEncode(schedule);
+    final response = await http.post(url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: body);
+    if (response.statusCode != 200) {
+      throw Error();
+    }
+    return;
+  }
+
+  //일정 수정
+  static void editSchedule(int studentID, Schedule schedule) async {
+    final url = Uri.parse('$baseUrl/schedule/$studentID');
+    final body = jsonEncode(schedule);
+    final response = await http.put(url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: body);
+    if (response.statusCode != 200) {
+      throw Error();
+    }
+    return;
+  }
+
+  //일정 검색
+  static Future<List<Schedule>> searchSchedules(
+      int studentID, String keyword) async {
+    final url = Uri.parse('$baseUrl/schedule/search/$studentID');
+
+    final response = await http.post(url, body: keyword);
+    if (response.statusCode != 200) {
+      throw Error();
+    }
+    final List<dynamic> schedules = jsonDecode(response.body);
+    List<Schedule> scheduleInstances =
+        schedules.map((schedule) => Schedule.fromJson(schedule)).toList();
+    return scheduleInstances;
+  }
+
+  //일정 삭제
+  static void deleteSchedules(int studentID, int scheduleID) async {
+    final url = Uri.parse('$baseUrl/schedule/$studentID/$scheduleID');
+    final response = await http.delete(url);
+    if (response.statusCode != 200) {
+      throw Error();
+    }
+    return;
+  }
+}
--- lib/schedule.dart
+++ lib/schedule.dart
@@ -1,6 +1,14 @@
 class Schedule {
-  final int scheduleId;
+  final int? scheduleId;
   final String event, location, month, day, time;
+
+  Schedule(
+      {this.scheduleId,
+      required this.event,
+      required this.location,
+      required this.month,
+      required this.day,
+      required this.time});
 
   Schedule.fromJson(Map<String, dynamic> json)
       : event = json['event'],
