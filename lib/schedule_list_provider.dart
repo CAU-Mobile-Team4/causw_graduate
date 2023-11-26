bb8bc7b90b2f32b170995ca738fa9aed8791a23c
manleKim <kk410910@naver.com>
2023-11-26 16:55:00 +0900

    일정 추가, 삭제 api 추가

--- lib/schedule_list_provider.dart
+++ lib/schedule_list_provider.dart
@@ -7,6 +7,8 @@ class ScheduleListProvider extends ChangeNotifier {
 
   List<Schedule> get scheduleListByDate => _scheduleListByDate;
 
+  void editSchedule(Schedule schedule) {}
+
   Future<void> updateScheduleList(int studentId) async {
     _scheduleListByDate = await ApiService.getSchedules(studentId);
     notifyListeners();
@@ -19,6 +21,47 @@ class ScheduleListProvider extends ChangeNotifier {
     } else {
       print('수정 실패');
     }
+    _scheduleListByDate
+        .removeWhere((element) => element.scheduleId == schedule.scheduleId);
+    _scheduleListByDate.add(schedule);
+
+    notifyListeners();
+  }
+
+  Future<void> deleteScheduleList(int scheduleId) async {
+    bool result = await ApiService.deleteSchedules(1, scheduleId);
+    if (result) {
+      print('추가 성공!');
+    } else {
+      print('추가 실패');
+    }
+    _scheduleListByDate
+        .removeWhere((element) => element.scheduleId == scheduleId);
+
+    notifyListeners();
+  }
+
+  Future<void> addScheduleList(Schedule schedule) async {
+    bool result = await ApiService.addSchedule(1, schedule);
+    if (result) {
+      print('추가 성공!');
+    } else {
+      print('추가 실패');
+    }
+    _scheduleListByDate.add(schedule);
+
+    notifyListeners();
+  }
+
+  Future<void> addScheduleListByNlp(String statement) async {
+    bool result = await ApiService.addScheduleByNlp(1, statement);
+    if (result) {
+      print('추가 성공!');
+    } else {
+      print('추가 실패');
+    }
+    updateScheduleList(1);
+
     notifyListeners();
   }
 }
