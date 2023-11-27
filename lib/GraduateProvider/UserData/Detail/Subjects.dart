d5307603cc696b31153970b30cb79ba1e5af3943
keylime7 <keylime7@cau.ac.kr>
2023-11-27 17:29:10 +0900

    feat: 프로바이더의 인스턴스 변수가 map 일 경우 그냥 set 메소드 못쓰는데 쓰던거, updateSubject 메소드로 대체

--- lib/GraduateProvider/UserData/Detail/Subjects.dart
+++ lib/GraduateProvider/UserData/Detail/Subjects.dart
@@ -14,23 +14,24 @@ class Subjects extends UserDataDetail {
   List<Map<String, dynamic>> get specializedElective => _specializedElective;
   Map<String, dynamic> get coreElective => _coreElective;
 
-  set major(List<Map<String, dynamic>> value) {
-    _major = value;
-    notifyListeners();
-  }
-
-  set bsm(List<Map<String, dynamic>> value) {
-    _bsm = value;
-    notifyListeners();
-  }
-
-  set specializedElective(List<Map<String, dynamic>> value) {
-    _specializedElective = value;
-    notifyListeners();
-  }
-
-  set coreElective(Map<String, dynamic> value) {
-    _coreElective = value;
+  void updateSubject(String subjectType, int index, String key, [String? value]) {
+    if(subjectType == 'major') {
+      if(value != null) {
+        _major[index][key] = !_major[index][key];
+      }
+    } else if(subjectType == 'bsm') {
+      if(value != null) {
+        _bsm[index][key] = !_bsm[index][key];
+      }
+    } else if(subjectType == 'specializedElective') {
+      if(value != null) {
+        _specializedElective[index][key] = !_specializedElective[index][key];
+      }
+    } else if(subjectType == 'coreElective') {
+      if(value != null) {
+        _coreElective[index][key] = !_coreElective[index][key];
+      }
+    }
     notifyListeners();
   }
 
