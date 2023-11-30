87096b0bb4c881eb8aacccc8049cbff84bcbabc0
SaeJoon <psae0714@naver.com>
2023-11-30 11:00:24 +0900

    핵교 띄우기

--- lib/GraduateUI/GEclassSelectionPage.dart
+++ lib/GraduateUI/GEclassSelectionPage.dart
@@ -1,7 +1,8 @@
+import 'package:causw_graduate/GraduateProvider/UserData/Detail/Subjects.dart';
 import 'package:causw_graduate/GraduateUI/ClassSelectionPage.dart';
 import 'package:causw_graduate/AppColor.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 class SelectGEclass extends StatefulWidget {
   const SelectGEclass({super.key});
 
@@ -14,6 +15,8 @@ class _SelectGEclassState extends State<SelectGEclass> {
   final List<bool> checkedClasses=List.generate(100, (index) => false);
   @override
   Widget build(BuildContext context) {
+    final subjects = Provider.of<Subjects>(context);
+
     return DefaultTabController(
       length: 2,
       child: Scaffold(
@@ -30,21 +33,19 @@ class _SelectGEclassState extends State<SelectGEclass> {
             automaticallyImplyLeading: false,
 
             backgroundColor: AppColor.background,
-            bottom: TabBar(tabs: [Tab(child: Text("핵심 교양",style: TextStyle(color: AppColor.main),)),Tab(child: Text("일반 교양",style: TextStyle(color: AppColor.main)))],
+            bottom: TabBar(tabs: [Tab(child: Text("핵심 교양",style: TextStyle(color: AppColor.main),)),Tab(child: Text("전문 교양",style: TextStyle(color: AppColor.main)))],
             ),
           ),
           body: TabBarView(children: [Expanded(
             child: Container(
                 child: ListView.builder(
-                  itemCount: classes.length,
+                  itemCount: (subjects.coreElective['convergence'] as List).length,
                   itemBuilder: (context,index){
                     return CheckboxListTile(
-                        title: Text(classes[index],style: TextStyle(color: AppColor.main,fontWeight: FontWeight.bold),),
-                        value: checkedClasses[index],
+                        title: Text('${subjects.coreElective['convergence'][index]['name']}',style: TextStyle(color: AppColor.main,fontWeight: FontWeight.bold),),
+                        value: subjects.coreElective['convergence'][index]['isSatisfied'],
                         onChanged: (value){
-                          setState(() {
-                            checkedClasses[index]=value!;
-                          });
+                          subjects.updateSubject('coreElective', index, 'isSatisfied');
                         }
                     );
                   },
