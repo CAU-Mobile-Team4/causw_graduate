4545acfe1ad01a5327b14da49c46df9bdeaf8d22
manleKim <kk410910@naver.com>
2023-11-30 09:40:16 +0900

    feat: 탭바 구현

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,54 @@ class MyApp extends StatelessWidget {
           primarySwatch: AppColor.createMaterialColor(AppColor.main),
           fontFamily: 'Nunito',
         ),
-        home: const Calendar(),
+        home: DefaultTabController(
+          length: 2,
+          child: Scaffold(
+            body: const TabBarView(
+              children: [
+                Calendar(),
+                Calendar(),
+              ],
+            ),
+            extendBodyBehindAppBar: true, // add this line
+
+            bottomNavigationBar: Container(
+              color: AppColor.background, //색상
+              child: Container(
+                height: 70,
+                padding: const EdgeInsets.only(bottom: 10, top: 5),
+                child: const TabBar(
+                  //tab 하단 indicator size -> .label = label의 길이
+                  //tab 하단 indicator size -> .tab = tab의 길이
+                  indicatorSize: TabBarIndicatorSize.label,
+                  //tab 하단 indicator color
+                  indicatorColor: AppColor.main,
+                  //tab 하단 indicator weight
+                  indicatorWeight: 2,
+                  //label color
+                  labelColor: AppColor.main,
+                  //unselected label color
+                  unselectedLabelColor: Colors.black38,
+                  labelStyle: TextStyle(
+                    fontSize: 13,
+                  ),
+                  tabs: [
+                    Tab(
+                      icon: Icon(
+                        Icons.checklist_outlined,
+                      ),
+                      text: 'Graduation Check List',
+                    ),
+                    Tab(
+                      icon: Icon(Icons.calendar_month_outlined),
+                      text: 'Calendar',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
